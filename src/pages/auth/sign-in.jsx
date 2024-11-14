b96bab25c76360ce015be3f8c4c1878ae0dbe9d8
hardikkukadiya <patel.satish56@gmail.com>
2024-11-14 16:18:26 +0530

    fix: reslove signin and signup responsive design issues

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -5,181 +5,101 @@ import { Link } from "react-router-dom";
 
 export function SignIn() {
   return (
-       <section className="bg-gray-100 min-h-screen py-10">
-            <div className="">
-          <div className="flex flex-wrap -mx-4">
-            <div className="w-full px-4">
-              <div className="max-w-[525px] mx-auto  text-center bg-white
-               rounded-lg
-               relative
-               overflow-hidden
-               py-16
-               px-10
-               sm:px-12
-               md:px-[60px]
-               ">
-                                <span className="text-2xl text-[#577fec]">Sign In</span>
-                <div className="mb-10 md:mb-10 text-center">
-                  <div                
-                    className="inline-block mx-auto">                                         
-                    <div className="flex justify-center items-center text-center gap-3">                  
-                    <img loading="lazy" src="/img/khanakLogo.png" alt=""  className="h-12 w-12"/>
-                      <span className="text-xl">Khanak Dashboard</span>
-                    </div>
-                                     </div>
-                                </div>
-                <form>
-                  <div className="mb-6">
-                    <input
-                      type="name"
-                      placeholder="Enter your name"
-                      className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-[#E9EDF4]
-                        py-3
-                        px-5
-                        bg-[#FCFDFE]
-                        text-base text-body-color
-                        placeholder-[#ACB6BE]
-                        outline-none
-                        focus-visible:shadow-none
-                        focus:border-primary
-                        "
-                    />
-                  </div>
-                  <div className="mb-6">
-                    <input
-                      type="text"
-                      placeholder="Enter your Email"
-                      className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-[#E9EDF4]
-                        py-3
-                        px-5
-                        bg-[#FCFDFE]
-                        text-base text-body-color
-                        placeholder-[#ACB6BE]
-                        outline-none
-                        focus-visible:shadow-none
-                        focus:border-primary
-                        "
-                    />
-                  </div>
-                  <div className="mb-6">
-                    <input
-                      type="password"
-                      placeholder="Enter Password Here"
-                      className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-[#E9EDF4]
-                        py-3
-                        px-5
-                        bg-[#FCFDFE]
-                        text-base text-body-color
-                        placeholder-[#ACB6BE]
-                        outline-none
-                        focus-visible:shadow-none
-                        focus:border-primary
-                        "
-                    />
-                  </div>
-                  <div className="mb-10">
-                    <input
-                      type="submit"
-                      value="Sign In"
-                      className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-primary
-                        py-3
-                        px-5
-                        bg-primary
-                        text-base text-white
-                        cursor-pointer
-                        transition
-                        bg-[#577fec]
-                        "
-                    />
-                  </div>
-                </form>
-                <p className="text-base mb-6 text-[#adadad]">Connect With</p>
-                <div className="flex justify-between -mx-2 mb-12">
-                  <div className="px-2 w-full">
-                    <div
-                       
-                      className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#4064AC]
-                        hover:bg-opacity-90
-                        "
-                    >
-                      <span><FaFacebookF size={20} color="white"/></span>
-                    </div>
-                  </div>
-                  <div className="px-2 w-full">
-                    <div
-                       
-                      className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#1C9CEA]
-                        hover:bg-opacity-90
-                        "
-                    >
-                      <span><FaTwitter size={20} color="white" /></span>
-                    </div>
-                  </div>
-                  <div className="px-2 w-full">
-                    <div
-                       
-                      className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#D64937]
-                        hover:bg-opacity-90
-                        "
-                    >
-                      <span><IoLogoGoogle size={20} color="white" /></span>
-                    </div>
-                  </div>
-                </div>
-                <a
-                  className="
-                  text-base
-                  inline-block
-                  mb-2
-                  text-[#adadad]
-                  hover:underline hover:text-blue-500" >
-                  Forget Password?
-                </a>
-                <p className="text-base text-[#adadad]">
-                  Not a member yet?
-                <Link to={"/auth/auth/sign-up"} className="text-primary hover:text-blue-500 hover:underline">
-                    Sign Up
-                  </Link>
-                </p>              
+    <section className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-[525px] p-5">
+        <div className="
+      mx-auto text-center bg-white rounded-lg relative overflow-hidden py-16 px-6
+      sm:px-10 md:px-[60px] shadow-lg
+    ">
+          <span className="text-2xl text-[#577fec]">Sign In</span>
+          <div className="mb-10 text-center">
+            <div className="flex justify-center items-center gap-3">
+              <img loading="lazy" src="/img/khanakLogo.png" alt="Khanak Logo" className="h-12 w-12" />
+              <span className="text-xl">Khanak Dashboard</span>
+            </div>
+          </div>
+          <form>
+            <div className="mb-6">
+              <input
+                type="name"
+                placeholder="Enter your name"
+                className="
+              w-full rounded-md border border-[#E9EDF4] py-3 px-5 bg-[#FCFDFE]
+              text-base text-body-color placeholder-[#ACB6BE] outline-none
+              focus-visible:shadow-none focus:border-primary
+            "
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                className="
+              w-full rounded-md border border-[#E9EDF4] py-3 px-5 bg-[#FCFDFE]
+              text-base text-body-color placeholder-[#ACB6BE] outline-none
+              focus-visible:shadow-none focus:border-primary
+            "
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                type="password"
+                placeholder="Enter Password Here"
+                className="
+              w-full rounded-md border border-[#E9EDF4] py-3 px-5 bg-[#FCFDFE]
+              text-base text-body-color placeholder-[#ACB6BE] outline-none
+              focus-visible:shadow-none focus:border-primary
+            "
+              />
+            </div>
+            <div className="mb-10">
+              <input
+                type="submit"
+                value="Sign In"
+                className="
+              w-full rounded-md py-3 px-5 bg-[#577fec] text-base text-white cursor-pointer
+              transition hover:bg-blue-600
+            "
+              />
+            </div>
+          </form>
+          <p className="text-base mb-6 text-[#adadad]">Connect With</p>
+          <div className="flex justify-between -mx-2 mb-12">
+            <div className="px-2 w-full">
+              <div className="
+            flex h-11 items-center justify-center rounded-md bg-[#4064AC] hover:bg-opacity-90
+          ">
+                <FaFacebookF size={20} color="white" />
+              </div>
+            </div>
+            <div className="px-2 w-full">
+              <div className="
+            flex h-11 items-center justify-center rounded-md bg-[#1C9CEA] hover:bg-opacity-90
+          ">
+                <FaTwitter size={20} color="white" />
+              </div>
+            </div>
+            <div className="px-2 w-full">
+              <div className="
+            flex h-11 items-center justify-center rounded-md bg-[#D64937] hover:bg-opacity-90
+          ">
+                <IoLogoGoogle size={20} color="white" />
               </div>
             </div>
           </div>
+          <a className="text-base inline-block mb-2 text-[#adadad] hover:underline hover:text-blue-500">
+            Forget Password?
+          </a>
+          <p className="text-base text-[#adadad]">
+            Not a member yet?
+            <Link to={"/auth/auth/sign-up"} className="text-primary hover:text-blue-500 hover:underline">
+              Sign Up
+            </Link>
+          </p>
         </div>
-      </section>
+      </div>
+    </section>
+
   );
 }
 
--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -8,156 +8,122 @@ import { Link } from "react-router-dom";
 
 export function SignUp() {
   return (
-    <section className="bg-gray-100 min-h-screen py-10">
-      <div className="">
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full px-4">
-            <div className=" max-w-[525px] mx-auto  text-center bg-white
-               rounded-lg
-               relative
-               overflow-hidden
-               py-16
-               px-10
-               sm:px-12
-               md:px-[60px]
-               ">
-              <div className="text-2xl text-[#577fec] mb-7">Join Us Today</div>              
-              <form>               
-                <div className="mb-6">
-                  <input
-                    type="text"
-                    placeholder="Enter your Email"
-                    className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-[#E9EDF4]
-                        py-3
-                        px-5
-                        bg-[#FCFDFE]
-                        text-base text-body-color
-                        placeholder-[#ACB6BE]
-                        outline-none
-                        focus-visible:shadow-none
-                        focus:border-primary
-                        "
-                  />
-                </div>
-                <div className="mb-6">
-                  <input
-                    type="password"
-                    placeholder="Enter Password Here"
-                    className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-[#E9EDF4]
-                        py-3
-                        px-5
-                        bg-[#FCFDFE]
-                        text-base text-body-color
-                        placeholder-[#ACB6BE]
-                        outline-none
-                        focus-visible:shadow-none
-                        focus:border-primary
-                        "
-                  />
-                </div>
-                <Checkbox
-                  label={
-                    <Typography
-                      variant="small"
-                      color="gray"
-                      className="flex items-center justify-start font-medium"
+    <section className="bg-gray-100 min-h-screen flex items-center justify-center py-10">
+      <div className="w-full max-w-[525px] px-4">
+        <div className="bg-white rounded-lg relative overflow-hidden py-16 px-6 sm:px-10 md:px-[60px] shadow-lg mx-auto text-center">
+          <div className="text-2xl text-[#577fec] mb-7">Join Us Today</div>
+
+          <form>
+            <div className="mb-6">
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                className="
+              w-full
+              rounded-md
+              border
+              border-[#E9EDF4]
+              py-3
+              px-5
+              bg-[#FCFDFE]
+              text-base text-body-color
+              placeholder-[#ACB6BE]
+              outline-none
+              focus-visible:shadow-none
+              focus:border-primary
+            "
+              />
+            </div>
+
+            <div className="mb-6">
+              <input
+                type="password"
+                placeholder="Enter Password Here"
+                className="
+              w-full
+              rounded-md
+              border
+              border-[#E9EDF4]
+              py-3
+              px-5
+              bg-[#FCFDFE]
+              text-base text-body-color
+              placeholder-[#ACB6BE]
+              outline-none
+              focus-visible:shadow-none
+              focus:border-primary
+            "
+              />
+            </div>
+
+            <div className="mb-6 flex items-center justify-start">
+              <Checkbox
+                label={
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="flex items-center justify-start font-medium"
+                  >
+                    I agree to the&nbsp;
+                    <Link
+                      to="/"
+                      className="font-normal text-black transition-colors hover:text-blue-500 underline"
                     >
-                      I agree the&nbsp;
-                      <Link
-                        to={"/"}
-                        className="font-normal text-black transition-colors hover:text-blue-500 underline"
-                      >
-                        Terms and Conditions
-                      </Link>
-                    </Typography>
-                  }
-                  containerProps={{ className: "-ml-2.5" }}
-                />
-                <div className="mb-10">
-                  <input
-                    type="submit"
-                    value="Register now"
-                    className="
-                        w-full
-                        rounded-md
-                        border
-                        bordder-primary
-                        py-3
-                        px-5
-                        mt-5
-                        bg-primary
-                        text-base text-white
-                        cursor-pointer
-                        transition
-                        bg-[#577fec]
-                        "
-                  />
-                </div>
-              </form>
-              <p className="text-base mb-6 text-[#adadad]">Connect With</p>
-              <div className="flex justify-between -mx-2 mb-12">
-                <div className="px-2 w-full">
-                  <div
+                      Terms and Conditions
+                    </Link>
+                  </Typography>
+                }
+                containerProps={{ className: "-ml-2.5" }}
+              />
+            </div>
 
-                    className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#4064AC]
-                        hover:bg-opacity-90
-                        "
-                  >
-                    <span><FaFacebookF size={20} color="white" /></span>
-                  </div>
-                </div>
-                <div className="px-2 w-full">
-                  <div
+            <div className="mb-10">
+              <input
+                type="submit"
+                value="Register now"
+                className="
+              w-full
+              rounded-md
+              border
+              border-primary
+              py-3
+              px-5
+              mt-5
+              bg-primary
+              text-base
+              text-white
+              cursor-pointer
+              transition
+              bg-[#577fec]
+            "
+              />
+            </div>
+          </form>
 
-                    className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#1C9CEA]
-                        hover:bg-opacity-90
-                        "
-                  >
-                    <span><FaTwitter size={20} color="white" /></span>
-                  </div>
-                </div>
-                <div className="px-2 w-full">
-                  <div
+          <p className="text-base mb-6 text-[#adadad]">Connect With</p>
+          <div className="flex justify-between gap-4 mb-12">
+            <div className="w-full">
+              <div className="flex h-11 items-center justify-center rounded-md bg-[#4064AC] hover:bg-opacity-90">
+                <FaFacebookF size={20} color="white" />
+              </div>
+            </div>
 
-                    className="
-                        flex
-                        h-11
-                        items-center
-                        justify-center
-                        rounded-md
-                        bg-[#D64937]
-                        hover:bg-opacity-90
-                        "
-                  >
-                    <span><IoLogoGoogle size={20} color="white" /></span>
-                  </div>
-                </div>
+            <div className="w-full">
+              <div className="flex h-11 items-center justify-center rounded-md bg-[#1C9CEA] hover:bg-opacity-90">
+                <FaTwitter size={20} color="white" />
+              </div>
+            </div>
+
+            <div className="w-full">
+              <div className="flex h-11 items-center justify-center rounded-md bg-[#D64937] hover:bg-opacity-90">
+                <IoLogoGoogle size={20} color="white" />
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
+
   );
 }
 
