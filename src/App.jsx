8a95b8e9310049ab7358ee3ecb157d135cba3b83
hardikkukadiya <patel.satish56@gmail.com>
2024-11-19 12:33:49 +0530

    fix: remove unused code from App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import { Dashboard, Auth } from "./layouts";
-// import "./App.css";
-// import { useState } from "react";
-
-
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/dashboard/*" element={<Dashboard />} />
-//       <Route path="/auth/*" element={<Auth />} />
-//       <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
-//     </Routes>
-//   );
-// }
-
-// export default App;
-
 import React, { Suspense, useState, useEffect, startTransition } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Dashboard, Auth } from './layouts';
