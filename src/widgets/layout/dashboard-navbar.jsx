d466a188b74a462a0c4c348b6e210fe8d1940944
hardikkukadiya <patel.satish56@gmail.com>
2024-11-27 15:39:34 +0530

    feat: Improve this toggle design in mobile device, need to update full height with overlay applied

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -78,7 +78,7 @@ export function DashboardNavbar() {
           <IconButton
             variant="text"
             color="blue-gray"
-            className="block xl:hidden"
+            className=""
             onClick={() => setOpenSidenav(dispatch, !openSidenav)}
           >
             <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
